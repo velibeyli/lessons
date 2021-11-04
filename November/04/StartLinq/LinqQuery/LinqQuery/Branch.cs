c25d173160e641b44ca0b8a0ca7165bfefe9da6e
velibeyli <valibayli91@gmail.com>
2021-11-04 21:54:09 +0400

    Linq query starts example

---
+++ November/04/StartLinq/LinqQuery/LinqQuery/Branch.cs
@@ -0,0 +1,8 @@
+﻿namespace LinqQuery
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ November/04/StartLinq/LinqQuery/LinqQuery/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqQuery
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
