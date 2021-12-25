ad2dc6d6cd126a129bae10c0e99eea9403b6bc9f
velibeyli <valibayli91@gmail.com>
2021-12-25 18:22:22 +0400

    folder 25 in December added

---
+++ december/25/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter language: ");
+            string langG = Console.ReadLine();
+
+            if (langG == "C#" || langG == "ASP.NET" || langG == "F#" || langG == "Java")
+            {
+                switch (langG)
+                {
+                    case "ASP.NET":
+                        Console.WriteLine("Microsoft");
+                        break;
+                    case "F#":
+                        Console.WriteLine("Microsoft");
+                        break;
+                    case "C#":
+                        Console.WriteLine("Microsoft");
+                        break;
+                    case "Java":
+                        Console.WriteLine("Oracle");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (langG == "German" || langG == "English (USA)" || langG == "English (GB)" || langG == "Turkish")
+            {
+                switch (langG)
+                {
+                    case "German":
+                        Console.WriteLine("+49");
+                        break;
+                    case "English (USA)":
+                        Console.WriteLine("+1");
+                        break;
+                    case "English (GB)":
+                        Console.WriteLine("+44");
+                        break;
+                    case "Turkish":
+                        Console.WriteLine("+90");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ december/25/switchcase2/switchcase2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchcase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string month = Console.ReadLine(); ;
+            //switch (month)
+            //{
+            //    case "Yanvar":
+            //        Console.WriteLine("01 " + month);
+            //        break;
+            //    case "Fevral":
+            //        Console.WriteLine("02 " + month);
+            //        break;
+            //    case "Mart":
+            //        Console.WriteLine("03 " + month);
+            //        break;
+            //    case "Aprel":
+            //        Console.WriteLine("04 " + month);
+            //        break;
+            //    case "May":
+            //        Console.WriteLine("05 " + month);
+            //        break;
+            //    case "Iyun":
+            //        Console.WriteLine("06 " + month);
+            //        break;
+            //    case "Iyul":
+            //        Console.WriteLine("07 " + month);
+            //        break;
+            //    case "Avqust":
+            //        Console.WriteLine("08 " + month);
+            //        break;
+            //    case "Sentyabr":
+            //        Console.WriteLine("09 " + month);
+            //        break;
+            //    case "Oktyabr":
+            //        Console.WriteLine("10 " + month);
+            //        break;
+            //    case "Noyabr":
+            //        Console.WriteLine("11 " + month);
+            //        break;
+            //    case "Dekabr":
+            //        Console.WriteLine("12 " + month);
+            //        break;
+            //    default:
+            //        Console.WriteLine("2021");
+            //        break;
+            //}
+
+            Console.WriteLine("a deyerini daxil edin: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("b deyerini daxil edin: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Etmek istediyiniz emeliyyat novunu daxil edin: ");
+            string symbol = Console.ReadLine();
+
+            switch (symbol)
+            {
+                case "+":
+                    Console.WriteLine(a + " + " + b + " = " +  (a + b) );
+                    break;
+                case "-":
+                    Console.WriteLine(a + " + " + b + " = " + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine(a + " * " + b + " = " + (a * b));
+                    break;
+                case "/":
+                    if (b == 0)
+                    {
+                        Console.WriteLine("bolen o ola bilmez");
+                    }
+                    else
+                    {
+                        Console.WriteLine(a + " / " + b + " = " + (a / b));
+                    }
+                    break;
+                default:
+                    break;
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
