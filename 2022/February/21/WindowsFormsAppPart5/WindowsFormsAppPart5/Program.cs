faf8740a77fa48620aaa66d3e0a2b500ba8f9062
Orkhan Farajov <orkhankhf@gmail.com>
2022-02-22 00:08:40 +0300

    WinForm part 5

--- 2022/February/21/WindowsFormsAppPart5/WindowsFormsAppPart5/Program.cs
+++ 2022/February/21/WindowsFormsAppPart5/WindowsFormsAppPart5/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsAppPart5
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BookStockAppListBox());
+            Application.Run(new GroupBoxForm());
         }
     }
 }
